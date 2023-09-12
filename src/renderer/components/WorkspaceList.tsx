3963b08d5fafdb9bf5c499e4dd7fa83fed315ae4
mfcmatheus <mfcmatheus@gmail.com>
2023-09-12 22:09:55 +0200

    Fixed grid layout bug

--- src/renderer/components/WorkspaceList.tsx
+++ src/renderer/components/WorkspaceList.tsx
@@ -8,8 +8,10 @@ function WorkspaceList(props: WorkspaceListProps) {
   const { children } = props
 
   return (
-    <div className="grid grid-cols-5 items-start gap-3 flex-grow basis-0 overflow-auto">
-      {children}
+    <div className="flex flex-grow basis-0 overflow-auto items-start">
+      <div className="grid grid-cols-5 justify-start items-start gap-3 w-full">
+        {children}
+      </div>
     </div>
   )
 }
